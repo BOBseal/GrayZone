a4a892ce8a0fc6f69961485e327db0dfb119644c
Bishal Kr Bhuyan <94212983+BOBseal@users.noreply.github.com>
2023-08-31 15:50:09 +0545

    Update page.tsx

--- grayzone_app/src/app/page.tsx
+++ grayzone_app/src/app/page.tsx
@@ -18,8 +18,8 @@ export default function Home() {
   
   return (
     <main className="flex h-full w-full">
-      <div className='h-screen w-screen bg-[#1D023C] -z-40 absolute top-0 right-0'/>
-      <Image src={'/Assets/bgImg.svg'} width={0} height={0} alt="GrayZone" className={`w-screen h-screen object-contain md:object-cover absolute top-0 left-0 -z-30`}/>
+      <div className='h-full w-full bg-[#1D023C] -z-40 absolute top-0 right-0'/>
+      <Image src={'/Assets/bgImg.svg'} width={0} height={0} alt="GrayZone" className={`w-screen h-screen object-cover md:object-cover absolute top-0 left-0 -z-30`}/>
 
       <div className={`${`bg-white text-black`} flex flex-col justify-center gap-24 items-center h-full w-full `}>
        
