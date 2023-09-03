49b5495fe9349fcf3cb68e60be12cb55eb0522d4
Bishal Kr Bhuyan <94212983+BOBseal@users.noreply.github.com>
2023-09-03 11:28:34 +0545

    Update page.tsx

--- grayzone_app/src/app/page.tsx
+++ grayzone_app/src/app/page.tsx
@@ -17,7 +17,7 @@ export default function Home() {
   //const {dark , setDark , handleThemeChange} = useContext(AppContext);
   
   return (
-    <main className="flex min-h-screen max-h-screen min-w-screen bg-[#1D023C] overflow-auto">
+    <main className="flex min-h-screen max-h-screen min-w-screen bg-[#1D023C] overflow-auto mt-[4.2rem]">
       <Image src={'/Assets/bgImg.svg'} width={0} height={0} alt="GrayZone" className={`w-screen h-screen object-cover md:object-cover absolute top-[2.5rem] left-0`}/>
 
       <div className={`${`bg-white text-black`} flex flex-col justify-center gap-24 items-center h-full w-full bg-[#1D023C]`}>
