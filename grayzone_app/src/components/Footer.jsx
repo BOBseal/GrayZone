46c7c4c8a3b40d11877c06647b6e41c8fa31066e
Bishal Kr Bhuyan <94212983+BOBseal@users.noreply.github.com>
2023-11-02 03:33:36 +0530

    Update Footer.jsx

--- grayzone_app/src/components/Footer.jsx
+++ grayzone_app/src/components/Footer.jsx
@@ -7,11 +7,11 @@ const Footer = () => {
   return (
     <div className='absolute h-[4.2rem] md:h-[4.5rem] w-full z-10 border-t-[2px] drop-shadow-m bg-gradient-to-bl to-[#b67ef5] from-[#1D023C] from-10% to-85%'>
         
-        <div className='h-full w-full items-center pl-[2rem] pr-[2rem] md:pl-[15rem] md:pr-[15rem] flex justify-between gap-1 text-[#FFFFFF]'>
-          <Link href={'https://twitter.com/grayzoneweb3'}><Image src={'/Assets/twitter.svg'} className="bg-[#9e59ff] border rounded-lg" width={40} height={40}/></Link>
-          <Link href={'https://t.me/grayzoneweb3'}><Image src={'/Assets/telegram.svg'} className="bg-[#9e59ff] border rounded-lg" width={40} height={40}/></Link>
-          <Link href={'https://mail.google.com/mail/u/0/#inbox?compose=DmwnWrRvwLvPLBHHfvdlBlWMMKtQJCBSVKQBwGJHlbCskmtvZZknScBxqSZrBszhbpHNfvspwmNG'}><Image src={'/Assets/mail.svg'} className="bg-[#9e59ff] border rounded-lg" width={40} height={40}/></Link>
-          <Link href={'https://www.linkedin.com/company/grayzoneweb3/'}><Image src={'/Assets/linkedin.svg'} className="bg-[#9e59ff] border rounded-lg" width={40} height={40}/></Link>
+        <div className='h-full w-full items-center pl-[2rem] pr-[2rem] md:pl-[15rem] md:pr-[15rem] flex justify-between gap-1 text-[#FFFFFF] drop-shadow-xl'>
+          <Link href={'https://twitter.com/grayzoneweb3'}><Image src={'/Assets/twitter.svg'} className="bg-[#9e59ff] border rounded-lg drop-shadow-xl" width={40} height={40}/></Link>
+          <Link href={'https://t.me/grayzoneweb3'}><Image src={'/Assets/telegram.svg'} className="bg-[#9e59ff] border rounded-lg drop-shadow-xl" width={40} height={40}/></Link>
+          <Link href={'https://mail.google.com/mail/u/0/#inbox?compose=DmwnWrRvwLvPLBHHfvdlBlWMMKtQJCBSVKQBwGJHlbCskmtvZZknScBxqSZrBszhbpHNfvspwmNG'}><Image src={'/Assets/mail.svg'} className="bg-[#9e59ff] border drop-shadow-xl rounded-lg" width={40} height={40}/></Link>
+          <Link href={'https://www.linkedin.com/company/grayzoneweb3/'}><Image src={'/Assets/linkedin.svg'} className="bg-[#9e59ff] border rounded-lg drop-shadow-xl" width={40} height={40}/></Link>
         </div>
     </div>       
   )
